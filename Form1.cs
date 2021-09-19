a8b26a25d2230edf38dbdac19bda825bc75fcc7f
StarDust <sid.kakroo@gmail.com>
2021-09-19 17:11:50 +0530

    final-commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Speech.Synthesis;
+
+
+namespace TesttoSpeech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Input = Interaction.InputBox("Abe bol", "i will speak what you type here", ".", 500, 300);
+            synth.SpeakAsync(Input);
+        }
+    }
+}
